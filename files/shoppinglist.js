90337238c41925a6ccff1900c260825984299302
Simon Goller <neosam@Simons-Air.fritz.box>
2017-10-01 18:51:26 +0200

    Initial commit

---
+++ files/shoppinglist.js
@@ -0,0 +1,112 @@
+(function () {
+    angular.module("shoppinglist", []);
+
+
+    angular.module("shoppinglist")
+        .directive("recipes", recipes);
+
+    function recipes() {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'recipes.html',
+            controllerAs: 'vm',
+            controller: RecipesController
+        };
+    }
+
+    RecipesController.$inject = ['$http']
+    function RecipesController($http) {
+        var vm = this;
+        vm.recipes = [{
+            name: 'Recipe 1'
+        }, {
+            name: 'Recipe 2'
+        }];
+        vm.shppinglist = [];
+        $http.get('../get-recipes')
+            .then(function (request) {
+                vm.recipes = request.data;
+            });
+        vm.newRecipe = {};
+        vm.add = add;
+
+        function add() {
+            $http.get('../add-recipe?name=' + vm.newRecipe.name);
+            vm.recipes.push(vm.newRecipe);
+            vm.newRecipe = {};
+        }
+    }
+
+    angular.module('shoppinglist').directive('shoppinglist', shoppinglist);
+
+    function shoppinglist() {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'shoppinglist.html',
+            controllerAs: 'vm',
+            controller: ShoppinglistController
+        }
+    }
+
+    ShoppinglistController.$inject = ['$http'];
+    function ShoppinglistController($http) {
+        var vm = this;
+        vm.list = [];
+        vm.newIngredient = "";
+        vm.selectedIngredient = null;
+        vm.ingredients = [];
+        vm.ingredientFilter = "";
+        vm.newAmount = 0;
+        vm.add = add;
+        vm.ingredientFilterFn = ingredientFilterFn;
+        vm.addIngredient = addIngredient;
+        vm.selectIngredient = selectIngredient;
+        $http.get('../get-shoppinglist')
+            .then(function (request) {
+                vm.list = request.data;
+            });
+        $http.get('../get-ingredients')
+            .then(function (request) {
+                vm.ingredients = request.data;
+            });
+
+        function add() {
+            var ingredientKey = vm.selectedIngredient.key;
+            var amount = vm.newAmount;
+            $http.get('../add-shoppinglist?ingredient_key=' + ingredientKey
+                + "&amount=" + amount).then(function (response) {
+                    vm.list.push({
+                        key: response.data,
+                        ingredient_key: ingredientKey,
+                        amount: amount,
+                        recipe_key: null
+                    });
+                });
+
+            vm.newIngredient = "";
+            vm.newAmount = 0;
+        }
+
+        function ingredientFilterFn() {
+            return _.filter(vm.ingredients, function (ingredient) {
+                return ingredient.name.search(vm.ingredientFilter) >= 0;
+            });
+        }
+
+        function addIngredient() {
+            $http.get('../add-ingredient?name=' + vm.ingredientFilter).then(function (response) {
+                vm.ingredients.push({
+                    name: vm.ingredientFilter,
+                    ingredient_key: response.data
+                });
+            });
+        }
+
+        function selectIngredient(ingredientKey) {
+            vm.selectedIngredient = ingredientKey;
+        }
+    }
+
+})();
---
+++ src/main.rs
@@ -0,0 +1,170 @@
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate shoppinglist;
+extern crate serde_json;
+extern crate uuid;
+#[macro_use] extern crate serde_derive;
+
+use rocket::State;
+use rocket::response::content::Content;
+use rocket::http::ContentType;
+use shoppinglist::*;
+use std::fs::File;
+use std::io::Read;
+
+use uuid::Uuid;
+
+use std::cell::RefCell;
+use std::sync::Mutex;
+
+type DefaultState = Mutex<RefCell<ShoppingList>>;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+#[get("/get-recipes")]
+fn get_recipes(shoppinglist: State<DefaultState>) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let shoppinglist = shoppinglist.borrow();
+    let recipes : Vec<Recipe>= shoppinglist.recipe_iter()
+        .map(|x| x.clone())
+        .collect();
+    serde_json::to_string(&recipes).unwrap()
+}
+
+#[derive(FromForm)]
+struct AddRecipeForm {
+    name: String
+}
+
+#[get("/add-recipe?<form>")]
+fn add_recipe(shoppinglist: State<DefaultState>, form: AddRecipeForm) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let mut shoppinglist = shoppinglist.borrow_mut();
+    let key = shoppinglist.generate_recipe(form.name);
+    serde_json::to_string(&key.key).unwrap()
+}
+
+#[derive(Serialize)]
+struct ShoppingListResponse {
+    pub key: Uuid,
+    pub name: String,
+    pub ingredient_key: Uuid,
+    pub amount: f32
+}
+
+#[get("/get-shoppinglist")]
+fn get_shoppinglist(shoppinglist: State<DefaultState>) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let shoppinglist = shoppinglist.borrow();
+    let recipes : Vec<ShoppingListResponse> = shoppinglist.shoppinglist_iter()
+        .map(|x : &ShoppingListItem| ShoppingListResponse {
+            key: x.key.clone(),
+            name: shoppinglist.ingredient(Key::new(x.ingredient_key)).name.clone(),
+            ingredient_key: x.ingredient_key.clone(),
+            amount: x.amount
+        })
+        .collect();
+    serde_json::to_string(&recipes).unwrap()
+}
+
+#[derive(FromForm)]
+struct AddShoppinglistForm {
+    ingredient_key: String,
+    amount: f32
+}
+
+#[get("/add-shoppinglist?<form>")]
+fn add_shoppinglist(shoppinglist: State<DefaultState>, form: AddShoppinglistForm) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let mut shoppinglist = shoppinglist.borrow_mut();
+    let ingredient_key = Uuid::parse_str(&form.ingredient_key).unwrap();
+    let key =
+        shoppinglist.insert_shoppinglist(Key::new(ingredient_key), form.amount);
+    serde_json::to_string(&key.key).unwrap()
+}
+
+#[derive(FromForm)]
+struct AddIngredientForm {
+    name: String
+}
+
+
+#[get("/add-ingredient?<form>")]
+fn add_ingredient(shoppinglist: State<DefaultState>, form: AddIngredientForm) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let mut shoppinglist = shoppinglist.borrow_mut();
+    let key = shoppinglist.insert_ingredient(form.name);
+    serde_json::to_string(&key.key).unwrap()
+}
+
+#[derive(FromForm)]
+struct AddIngredientToRecipeForm {
+    recipe_key: String,
+    ingredient_key: String,
+    amount: f32
+}
+
+#[get("/get-ingredients")]
+fn get_ingredients(shoppinglist: State<DefaultState>) -> String {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let shoppinglist = shoppinglist.borrow();
+    let result : Vec<Ingredient> = shoppinglist
+        .ingredient_iter()
+        .map(|x| x.clone())
+        .collect();
+    serde_json::to_string(&result).unwrap()
+}
+
+#[get("/add-ingredient-to-recipe?<form>")]
+fn add_ingredient_to_recipe(shoppinglist: State<DefaultState>, form: AddIngredientToRecipeForm) -> &str {
+    let shoppinglist = shoppinglist.lock().unwrap();
+    let mut shoppinglist = shoppinglist.borrow_mut();
+    let recipe = shoppinglist.recipe_mut(Key::new(Uuid::parse_str(&form.recipe_key).unwrap()));
+    recipe.add_ingredient(Key::new(Uuid::parse_str(&form.ingredient_key).unwrap()), form.amount);
+    "true"
+}
+
+#[get("/file/<path>")]
+fn file(path: String) -> Content<Vec<u8>> {
+    let mut result = Vec::new();
+    let path = format!("files/{}", path);
+    File::open(path.clone()).unwrap().read_to_end(&mut result);
+    let content_type = if path.ends_with(".html") {
+            ContentType::new("text", "html")
+        } else if (path.ends_with(".js")) {
+            ContentType::new("text", "javascript")
+        } else if (path.ends_with(".css")) {
+            ContentType::new("text", "css")
+        } else if (path.ends_with(".jpg")) {
+            ContentType::new("image", "jpeg")
+        } else if (path.ends_with(".png")) {
+            ContentType::new("image", "png")
+        } else {
+            ContentType::new("text", "plain")
+        };
+    Content(content_type, result)
+}
+
+
+fn main() {
+    let r = rocket::ignite();
+    let shoppinglist = ShoppingList::new();
+    let r = r.manage(Mutex::new(RefCell::new(shoppinglist)));
+    let r = r.mount("/", routes![
+        index,
+        get_recipes,
+        add_recipe,
+        add_ingredient,
+        get_ingredients,
+        add_ingredient_to_recipe,
+        get_shoppinglist,
+        add_shoppinglist,
+        file]);
+    r.launch();
+}
